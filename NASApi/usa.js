cfa4040f105a0aba058d9ba9e88a66825bbdd23d
Leo Camacho <leandrocg@consultant.com>
2020-08-02 07:09:39 -0600

    Update usa.js

--- NASApi/usa.js
+++ NASApi/usa.js
@@ -53,7 +53,7 @@ window.onload = function () {
       format: 'image/jpeg',
       matrixSet: 'EPSG4326_250m',
       tileGrid: new ol.tilegrid.WMTS({
-        origin: [-180, 80],
+        origin: [-192, 94],
         resolutions: [
           0.5625,
           0.28125,
