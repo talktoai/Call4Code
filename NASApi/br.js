49b0df20d80417693d6ae0608e0c8ad63bbe36ee
Leo Camacho <leandrocg@consultant.com>
2020-08-02 07:10:33 -0600

    Update br.js

--- NASApi/br.js
+++ NASApi/br.js
@@ -53,7 +53,7 @@ window.onload = function () {
       format: 'image/jpeg',
       matrixSet: 'EPSG4326_250m',
       tileGrid: new ol.tilegrid.WMTS({
-        origin: [-180, 80],
+        origin: [-220, 120],
         resolutions: [
           0.5625,
           0.28125,
